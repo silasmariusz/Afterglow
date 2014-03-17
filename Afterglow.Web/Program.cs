fdcec40c7a7bc354c768dbabd68858a29e11eee0
silasmariusz <silas@qnapclub.pl>
2014-03-17 08:45:44 +0100

    Console minimize to Tray Icon

--- Afterglow.Web/Program.cs
+++ Afterglow.Web/Program.cs
@@ -8,6 +8,15 @@ using ServiceStack.Logging;
 using ServiceStack.Logging.Support.Logging;
 using Afterglow.Web.Host;
 
+// trayIcon
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using System.Drawing;
+// show/hide console app
+using System.Runtime.InteropServices;
+using System.Threading;
+
 namespace Afterglow.Web
 {
     class Program
@@ -20,33 +29,74 @@ namespace Afterglow.Web
             }
         }
         private static AfterglowRuntime _runtime;
-                
+
+
+        public static ContextMenu menu;
+        public static MenuItem mnuExit;
+        public static NotifyIcon notificationIcon;
+
+
+        private static AppHost _appHost;
+
+        
         static void Main(string[] args)
         {
+
+            // Create Tray Icon
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+
+            Stream iconResourceStream = currentAssembly.GetManifestResourceStream("Afterglow.Web.Resources.Icon.LED_light-16.ico");
+
+            Thread notifyThread = new Thread(
+                delegate()
+                {
+                    menu = new ContextMenu();
+                    mnuExit = new MenuItem("E&xit");
+                    menu.MenuItems.Add(0, mnuExit);
+
+                    notificationIcon = new NotifyIcon()
+                    {
+                        Icon = new Icon(iconResourceStream),
+                        ContextMenu = menu,
+                        Text = typeof (Program).Assembly.GetName().Name
+                    };
+
+                    notificationIcon.MouseClick += new MouseEventHandler(notificationIcon_Click);
+                    mnuExit.Click += new EventHandler(mnuExit_Click);
+
+                    notificationIcon.Visible = true;
+                    Application.Run();
+                }
+                );
+
+            notifyThread.Start();
+            // end: Tray Icon
+
 
             _runtime = new AfterglowRuntime();
             
             LogManager.LogFactory = new ConsoleLogFactory();
 
-            using (var appHost = new AppHost())
+            //using (var appHost = new AppHost())
+            _appHost = new AppHost();
             {
                 Console.WriteLine("Starting Afterglow runtime...");
                 //_runtime.Start();
 
                 Console.WriteLine("Afterglow runtime started.");
 
-                appHost.Init();
+                _appHost.Init();
                 string host = String.Format("http://localhost:{0}/", _runtime.Setup.Port);
                 if (args.Length > 0)
                     host = String.Format("http://{0}:{1}/", args[0], _runtime.Setup.Port);
                 
-                appHost.Start(host);
+                _appHost.Start(host);
 
                 Console.WriteLine(host);
                 
-                Console.WriteLine("Press <enter> to exit.");
-                Console.ReadLine();
-                appHost.Stop();
+                //Console.WriteLine("Press <enter> to exit.");
+                //Console.ReadLine();
+                //appHost.Stop();
 
             }
 
@@ -71,6 +121,51 @@ namespace Afterglow.Web
             //_runtime.Setup.Profiles.Add(profile);
             //_runtime.Setup.ConfiguredLightSetupPlugins.Add(.FirstOrDefault().OLDPostProcessPlugins.FirstOrDefault());
             //_runtime.Save();
+
+            ShowWindow(ThisConsole, 0);
         }
+
+
+        private static void mnuExit_Click(object sender, EventArgs e)
+        {
+            _appHost.Stop();
+
+            notificationIcon.Visible = false;
+
+//#if DEBUG
+            ShowWindow(ThisConsole, 1);
+            Console.WriteLine("Exiting...");
+            //Console.WriteLine("Press <enter> to exit.");
+            //Console.ReadLine();
+//#endif
+
+            Application.Exit();
+            Environment.Exit(0);
+
+            //Environment.Exit(1);
+        }
+
+        private static void notificationIcon_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                //reserve right click for context menu
+                showWindow = ++showWindow % 2;
+                ShowWindow(ThisConsole, showWindow);
+            }
+        }
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+
+        private static IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private static Int32 showWindow = 0; //0 - SW_HIDE - Hides the window and activates another window.
+
     }
 }
